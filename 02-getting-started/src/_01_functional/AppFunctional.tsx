b2fe20ce341494c6c96d2ecca07113d13b1da689
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-01-26 21:57:36 +0100

    02 getting started

---
+++ 02-getting-started/src/_01_functional/AppFunctional.tsx
@@ -0,0 +1,11 @@
+import "./../App.css";
+
+function AppFunction() {
+  return (
+    <div className="App">
+      <h1>Hello React TypeScript: functional</h1>
+    </div>
+  );
+}
+
+export default AppFunction;
---
+++ 02-getting-started/src/_02_class/AppClass.tsx
@@ -0,0 +1,12 @@
+import "./../App.css";
+import { Component } from "react";
+
+export default class AppClass extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React TypeScript: class</h1>
+      </div>
+    );
+  }
+}
